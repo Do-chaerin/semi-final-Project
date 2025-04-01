dd4146741e7a2fdf9a49ff70324ed18b3aef39a9
Do-chaerin <swin3321@gmail.com>
2025-04-01 17:35:13 +0900

    본프로젝트 코딩시작

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Notice from "./components/Notice.vue";
+import Review from "./components/Review.vue";
+</script>
+
+<template>
+  <Review />
+</template>
+
+<style scoped></style>
---
+++ src/components/Review.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+<template>
+  <div class="st_wrap">
+    <!-- 이미지배너 -->
+    <div class="st_reviewbanner">
+      <img src="/image/st_reviewbanner.jpg" alt="리뷰이벤트" />
+    </div>
+    <div class="st_reviewphoto"></div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@use "@/assets/Main.scss" as *;
+.st_wrap {
+  width: 700px;
+  display: flex;
+}
+//리뷰배너
+.st_reviewbanner {
+  justify-content: center;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
